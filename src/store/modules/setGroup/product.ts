389387dbae875376584a43bae32effbc9c79bb9c
Cai <2945245791@qq.com>
2025-06-24 22:09:01 +0800

    fix: 完善产品管理

---
+++ src/store/modules/setGroup/product.ts
@@ -0,0 +1,145 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import $Message from '@/components/Message';
+import $Notification from '@/components/Notification';
+import { reqProductList, reqAddProduct, reqUpdateProduct, reqUnitList } from '@/api/setGroup/product';
+import { ReponseCode, ReponseCodeMeaning } from '@/enums/response';
+
+export const useProductStore = defineStore('Product', () => {
+  // 是否允许折扣
+  const isDiscount: any = { 0: '允许', 1: '不允许' };
+
+  // 搜索参数
+  const searchParams = ref({
+    storeId: 0,
+    productName: '',
+    productStatus: 0,
+  });
+
+  // 单位
+  const unitOptions: any = ref([]);
+  const setUnitList = async () => {
+    try {
+      // const data = await reqUnitList();
+      // unitOptions.value = data;
+      unitOptions.value = [
+        {
+          value: 0,
+          label: '盒',
+        },
+        {
+          value: 1,
+          label: '个',
+        },
+        {
+          value: 2,
+          label: '瓶',
+        },
+      ];
+    } catch (error) {
+      $Message.error('获取产品单位列表失败');
+    }
+  };
+
+  // 产品
+  const productList: any = ref([]);
+  const setProductList = async () => {
+    try {
+      // 获取产品单位列表
+      setUnitList();
+      // 获取产品列表
+      const data: any = (await reqProductList(searchParams.value)).data;
+      // 处理数据
+      productList.value = data.map((item: any) => {
+        item.isDiscountStr = isDiscount[item.isDiscount] || '未知';
+        item.unit = unitOptions.value.find((unit: any) => unit.value === item.unit)?.label || '未知';
+        return item;
+      });
+    } catch (error) {
+      $Message.error('获取产品列表失败');
+    }
+  };
+
+  const updateProduct = async (data: any) => {
+    // 浅拷贝避免修改原数据
+    data = { ...data };
+    // 将数据中的提成比例转为小数
+    if (data.productCommissionValue > 1) {
+      data.productCommissionValue = data.productCommissionValue / 100;
+    }
+
+    // 如果没有产品状态属性赋默认值
+    data.productStatus = data?.productStatus || 0;
+    try {
+      // 发送请求
+      let res: any = {};
+      if (data?.id) {
+        res = await reqUpdateProduct(data);
+      } else {
+        res = await reqAddProduct(data);
+      }
+      if (res.code === ReponseCode.SUCCESS) {
+        $Notification.success(res.data); // 显示成功消息
+        setProductList(); // 重新获取数据
+        return true;
+      } else {
+        $Notification.error(res.data); // 显示错误消息
+        return false;
+      }
+    } catch (error) {
+      console.error(error);
+      $Message.error(ReponseCodeMeaning.FAIL);
+    }
+  };
+  const updateProductStatus = async (data: any) => {
+    data = { ...data };
+    data.productStatus = data.productStatus === 0 ? 1 : 0;
+    updateProduct(data);
+  };
+
+  // 表单数据
+  const formData: any = ref({});
+
+  // 重置表单数据模型
+  const resetFormData = () => {
+    formData.value = {
+      id: 0,
+      remark: '',
+      productName: '',
+      productEncode: '',
+      productPrice: 0,
+      vipProductPrice: 0,
+      isDiscount: 0,
+      commissioinType: 1,
+      productCommissionValue: 0,
+      productCommissionPrice: 0,
+      productCommissionValueType: 0,
+      productStatus: 0,
+      unit: 0,
+    };
+  };
+
+  // 表单验证规则
+  const formRules = {
+    productEncode: [{ required: false, message: '请输入产品编码', trigger: 'blur' }],
+    productName: [{ required: true, message: '请输入产品名称', trigger: 'blur' }],
+    productPrice: [{ required: true, message: '请输入产品价格', trigger: 'blur' }],
+    vipProductPrice: [{ required: true, message: '请输入会员价格', trigger: 'blur' }],
+    productCommissionValue: [{ required: false, message: '请输入提成价格', trigger: 'blur' }],
+    productCommissionPrice: [{ required: false, message: '请输入提成比例', trigger: 'blur' }],
+    productCommissionValueType: [{ required: false, message: '请选择提成价格类型', trigger: 'blur' }],
+  };
+
+  return {
+    searchParams,
+    productList,
+    setProductList,
+    updateProduct,
+    updateProductStatus,
+    unitOptions,
+    setUnitList,
+    formData,
+    resetFormData,
+    formRules,
+  };
+});
--- src/views/setGroup/psMain/productManager/index.vue
+++ src/views/setGroup/psMain/productManager/index.vue
@@ -6,15 +6,32 @@
         <el-button type="primary" @click="showDrawer(0)" class="add-button">添加产品</el-button>
       </div>
       <div class="search-container">
+        <div class="search-item" v-if="false">
+          <label for="staffStatus" class="search-label">选择店铺：</label>
+          <el-select
+            v-model="productStore.searchParams.storeId"
+            id="staffStatus"
+            style="width: 120px"
+            placeholder="Select"
+          >
+            <el-option v-for="item in statusOptions" :key="item.value" :label="item.label" :value="item.value" />
+          </el-select>
+        </div>
         <div class="search-item">
           <label for="staffStatus" class="search-label">商品状态：</label>
-          <el-select v-model="searchParams.productStatus" id="staffStatus" style="width: 120px" placeholder="Select">
+          <el-select
+            v-model="productStore.searchParams.productStatus"
+            id="staffStatus"
+            style="width: 120px"
+            placeholder="Select"
+          >
             <el-option v-for="item in statusOptions" :key="item.value" :label="item.label" :value="item.value" />
           </el-select>
         </div>
         <div class="search-item">
           <el-input
-            v-model="searchParams.productName"
+            v-model="productStore.searchParams.productName"
+            @keydown.enter="search"
             :prefix-icon="Search"
             placeholder="编码|产品名称"
             class="search-input"
@@ -29,25 +46,34 @@
 
     <!-- 表格组件 -->
     <Card class="table-card">
-      <Table :data="tableData" :border="true" :stripe="true" class="table-main">
+      <Table
+        :data="productStore.productList"
+        :border="true"
+        :stripe="true"
+        :row-class-name="getRowClassName"
+        class="table-main"
+      >
         <el-table-column prop="productName" label="产品" min-width="120" />
         <el-table-column prop="productEncode" label="编码" min-width="60" />
         <el-table-column prop="unit" label="单位/规格" min-width="100" />
         <el-table-column prop="productPrice" label="标准价(元)" min-width="100" />
         <el-table-column prop="vipProductPrice" label="会员价(元)" min-width="100" />
-        <el-table-column prop="isDiscount" label="参与折扣卡打折" min-width="130" />
+        <el-table-column prop="isDiscountStr" label="参与折扣卡打折" min-width="130" />
         <el-table-column label="操作" min-width="150">
           <template #default="scope">
-            <div>
-              <el-button link type="info" @click="showDrawer(2, scope.row)">更多</el-button>
-              <el-button link type="primary" :disabled="!!scope.row.productStatus" @click="showDrawer(1, scope.row)">
-                编辑
-              </el-button>
-              <el-button link type="warning" v-if="scope.row.productStatus" @click="updateProductStatus(scope.row)">
-                启用
-              </el-button>
-              <el-button link type="warning" v-else @click="showConfirm(scope.row)">禁用</el-button>
-            </div>
+            <el-button link type="info" @click="showDrawer(2, scope.row)">更多</el-button>
+            <el-button link type="primary" :disabled="!!scope.row.productStatus" @click="showDrawer(1, scope.row)">
+              编辑
+            </el-button>
+            <el-button
+              link
+              type="warning"
+              v-if="scope.row.productStatus"
+              @click="productStore.updateProductStatus(scope.row)"
+            >
+              启用
+            </el-button>
+            <el-button link type="warning" v-else @click="showConfirm(scope.row)">禁用</el-button>
           </template>
         </el-table-column>
       </Table>
@@ -58,30 +84,31 @@
   <Drawer v-model="drawer.visible" :title="drawer.title" @close="handleDrawerClose">
     <!-- 表单 -->
     <Form
-      :model="formData"
-      :rules="formRules"
+      :model="productStore.formData"
+      :rules="productStore.formRules"
       :showButtons="!drawer.disabled"
+      :disabled="drawer.disabled"
       @submit="handleFormSubmit"
       @reset="handleFormReset"
     >
       <!-- 产品编码 -->
       <el-form-item label="产品编码" prop="productEncode">
-        <el-input v-model="formData.productEncode" :disabled="drawer.disabled" placeholder="请输入产品编码" />
+        <el-input v-model="productStore.formData.productEncode" placeholder="请输入产品编码" />
       </el-form-item>
       <!-- 产品名称 -->
       <el-form-item label="产品名称" prop="productName">
-        <el-input v-model="formData.productName" :disabled="drawer.disabled" placeholder="请输入产品名称" />
+        <el-input v-model="productStore.formData.productName" placeholder="请输入产品名称" />
       </el-form-item>
       <!-- 商品单位 -->
       <el-form-item label="商品单位" prop="ProductUnit">
         <div style="width: 100%; display: flex; gap: 10px">
-          <el-select
-            v-model="formData.unit"
-            :disabled="drawer.disabled"
-            style="width: 200px"
-            placeholder="选择商品单位"
-          >
-            <el-option v-for="item in unitOptions" :key="item.value" :label="item.label" :value="item.value" />
+          <el-select v-model="productStore.formData.unit" style="width: 200px" placeholder="选择商品单位">
+            <el-option
+              v-for="item in productStore.unitOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
           </el-select>
           <div><el-button link type="primary" @click="unitManger">单位管理</el-button></div>
         </div>
@@ -90,21 +117,11 @@
       <el-form-item label="价格设置">
         <Card>
           <el-form-item label="标准价：" prop="productPrice" style="margin-bottom: 15px">
-            <el-input-number
-              size="small"
-              v-model="formData.productPrice"
-              :controls="false"
-              :disabled="drawer.disabled"
-            />
+            <el-input-number size="small" v-model="productStore.formData.productPrice" :controls="false" />
             &nbsp;元
           </el-form-item>
           <el-form-item label="会员价：" prop="vipProductPrice">
-            <el-input-number
-              size="small"
-              v-model="formData.vipProductPrice"
-              :controls="false"
-              :disabled="drawer.disabled"
-            />
+            <el-input-number size="small" v-model="productStore.formData.vipProductPrice" :controls="false" />
             &nbsp;元
           </el-form-item>
         </Card>
@@ -112,45 +129,34 @@
       <!-- 允许打折 -->
       <el-form-item label="允许打折" prop="isDiscount">
         <el-switch
-          v-model="formData.isDiscount"
+          v-model="productStore.formData.isDiscount"
           style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
           :active-value="0"
           :inactive-value="1"
-          :disabled="drawer.disabled"
         />
       </el-form-item>
       <!-- 提成类型 -->
       <el-form-item label="提成类型" prop="commissioinType">
-        <el-radio-group v-model="formData.commissioinType" :disabled="drawer.disabled">
+        <el-radio-group v-model="productStore.formData.commissioinType">
           <el-radio :value="1" :border="true">固定金额</el-radio>
           <el-radio :value="0" :border="true">比例提成</el-radio>
         </el-radio-group>
       </el-form-item>
       <!-- 固定金额 -->
-      <template v-if="formData.commissioinType === 1">
+      <template v-if="productStore.formData.commissioinType === 1">
         <el-form-item label="提成值" prop="productCommissionPrice">
-          <el-input-number
-            size="small"
-            v-model="formData.productCommissionPrice"
-            :controls="false"
-            :disabled="drawer.disabled"
-          />
+          <el-input-number size="small" v-model="productStore.formData.productCommissionPrice" :controls="false" />
           &nbsp;元
         </el-form-item>
       </template>
       <!-- 比例提成 -->
-      <template v-if="formData.commissioinType === 0">
+      <template v-if="productStore.formData.commissioinType === 0">
         <el-form-item label="提成比例" prop="productCommissionValue" style="margin-bottom: 15px">
-          <el-input-number
-            size="small"
-            v-model="formData.productCommissionValue"
-            :controls="false"
-            :disabled="drawer.disabled"
-          />
+          <el-input-number size="small" v-model="productStore.formData.productCommissionValue" :controls="false" />
           &nbsp;%
         </el-form-item>
         <el-form-item label="价格类型" prop="productCommissionValueType">
-          <el-select v-model="formData.productCommissionValueType" style="width: 200px" :disabled="drawer.disabled">
+          <el-select v-model="productStore.formData.productCommissionValueType" style="width: 200px">
             <el-option v-for="item in commissionOptions" :key="item.value" :label="item.label" :value="item.value" />
           </el-select>
         </el-form-item>
@@ -158,20 +164,20 @@
       <!-- 其他描述 -->
       <el-form-item label="其他描述">
         <el-input
-          v-model="formData.remark"
+          v-model="productStore.formData.remark"
           style="width: 240px"
           :autosize="{ minRows: 2, maxRows: 4 }"
-          :disabled="drawer.disabled"
           type="textarea"
           placeholder="请输入产品描述"
         />
       </el-form-item>
-
-      <!-- 抽屉操作按钮 -->
-      <!-- <template #buttons>
-        <el-button @click="drawer.visible = false">取消</el-button>
-      </template> -->
     </Form>
+    <!-- 抽屉操作按钮 -->
+    <template v-if="drawer.disabled">
+      <div class="drawer-buttons">
+        <el-button @click="drawer.visible = false">取消</el-button>
+      </div>
+    </template>
   </Drawer>
 </template>
 
@@ -179,14 +185,14 @@
 import { Search } from '@element-plus/icons-vue';
 import { ref, onMounted, inject } from 'vue';
 
-// 导入请求方法
-import { reqProductList, reqAddProduct, reqUpdateProduct } from '@/api/setGroup/product';
 // 导入枚举数据
-import { statusOptions, commissionOptions, ReponseCode } from '@/enums/index';
+import { statusOptions, commissionOptions } from '@/enums/index';
+// 引入产品数据仓库
+import { useProductStore } from '@/store/modules/setGroup/product';
+const productStore = useProductStore();
 
 // 引入消息提示组件
 const $MessageBox: any = inject('$MessageBox');
-const $Notification: any = inject('$Notification');
 
 defineProps({
   title: {
@@ -195,144 +201,41 @@ defineProps({
   },
 });
 
-// #region 初始化渲染
-const tableData: any = ref([]);
-const unitOptions: any = ref([]);
-
 onMounted(() => {
-  getProductList();
-  unitOptions.value = [
-    {
-      value: 0,
-      label: '盒',
-    },
-    {
-      value: 1,
-      label: '个',
-    },
-    {
-      value: 2,
-      label: '瓶',
-    },
-  ];
+  productStore.setProductList();
 });
-// #endregion
 
-// #region 数据交互
+// #region 事件处理
 
-// 初始化
-const getProductList = async () => {
-  const res = await reqProductList(searchParams.value);
-  tableData.value = res.data;
+// 搜索产品
+const search = () => {
+  productStore.setProductList();
 };
 
-// 禁用
+// 禁用产品
 const showConfirm = async ($row: any) => {
   const result = await $MessageBox.confirm({
     title: '确认操作',
     message: `你确定要禁用产品【${$row.productName}】吗？`,
     type: 'warning',
   });
-  result && updateProductStatus($row);
+  result && productStore.updateProductStatus($row);
 };
 
-// 修改
-const updateProductStatus = async ($row: any) => {
-  update($row, (data: any) => {
-    data.productStatus = data.productStatus === 0 ? 1 : 0;
-    return reqUpdateProduct(data);
-  });
+// 表单提交
+const handleFormSubmit = async (model: any) => {
+  const result = await productStore.updateProduct(model);
+  result && (drawer.value.visible = false);
 };
 
-// 通用的修改函数
-const update = async (data: any, callback: Function) => {
-  // 浅拷贝避免修改原数据
-  data = { ...data };
-  // 将数据中的提成比例转为小数
-  if (data.productCommissionValue > 1) {
-    data.productCommissionValue = data.productCommissionValue / 100;
-  }
-
-  // 如果没有产品状态属性赋默认值
-  data.productStatus = data?.productStatus || 0;
-
-  // 发送请求
-  const res: any = await callback(data);
-  if (res.code === ReponseCode.SUCCESS) {
-    $Notification.success(res.data); // 显示成功消息
-    drawer.value.visible = false; // 关闭抽屉
-    getProductList(); // 重新获取数据
-  } else {
-    $Notification.error(res.data); // 显示错误消息
-  }
+// 表单重置
+const handleFormReset = () => {
+  productStore.resetFormData();
 };
 
 // 单位管理
 const unitManger = () => {
   console.log('单位管理');
-};
-
-// #endregion
-
-// #region 搜索模块
-const searchParams = ref({
-  productName: '',
-  productStatus: 0,
-});
-
-const search = () => {
-  getProductList();
-};
-// #endregion
-
-// #region 表单模块
-
-// 表单数据
-const formData: any = ref({});
-
-// 重置表单数据模型
-const resetFormData = () => {
-  formData.value = {
-    id: 0,
-    remark: '',
-    productName: '',
-    productEncode: '',
-    productPrice: 0,
-    vipProductPrice: 0,
-    isDiscount: 0,
-    commissioinType: 1,
-    productCommissionValue: 0,
-    productCommissionPrice: 0,
-    productCommissionValueType: 0,
-    productStatus: 0,
-    unit: 0,
-  };
-};
-
-// 表单验证规则
-const formRules = {
-  productEncode: [{ required: false, message: '请输入产品编码', trigger: 'blur' }],
-  productName: [{ required: true, message: '请输入产品名称', trigger: 'blur' }],
-  productPrice: [{ required: true, message: '请输入产品价格', trigger: 'blur' }],
-  vipProductPrice: [{ required: true, message: '请输入会员价格', trigger: 'blur' }],
-  productCommissionValue: [{ required: false, message: '请输入提成价格', trigger: 'blur' }],
-  productCommissionPrice: [{ required: false, message: '请输入提成比例', trigger: 'blur' }],
-  productCommissionValueType: [{ required: false, message: '请选择提成价格类型', trigger: 'blur' }],
-};
-
-/**
- * 表单提交
- * @param model 表单数据
- */
-const handleFormSubmit = async (model: any) => {
-  update(model, (data: any) => {
-    return data?.id ? reqUpdateProduct(data) : reqAddProduct(data);
-  });
-};
-
-// 表单重置触发
-const handleFormReset = () => {
-  resetFormData();
 };
 // #endregion
 
@@ -346,7 +249,7 @@ const drawer: any = ref({
 // 抽屉标题
 const drawerTitles = ['新增产品信息', '修改产品信息', '产品信息'];
 
-// 添加产品
+// 打开抽屉
 const showDrawer = (titleIndex: number, $row: any = {}) => {
   // 修改抽屉标题
   drawer.value.title = drawerTitles[titleIndex];
@@ -355,6 +258,7 @@ const showDrawer = (titleIndex: number, $row: any = {}) => {
 
   // 如果点击更多 禁用表单
   titleIndex === 2 && (drawer.value.disabled = true);
+  titleIndex === 0 && productStore.setUnitList();
 
   // 浅拷贝防止直接操作原对象
   $row = { ...$row };
@@ -364,20 +268,29 @@ const showDrawer = (titleIndex: number, $row: any = {}) => {
   }
   // 表单数据回显
   if ($row?.id) {
-    formData.value = $row;
+    productStore.formData = $row;
   } else {
-    resetFormData();
+    productStore.resetFormData();
   }
 };
 
 // 关闭抽屉触发
 const handleDrawerClose = () => {
-  // 当抽屉关闭时重置表单
-  resetFormData();
-  // 去除预览禁用
-  drawer.value.disabled = false;
+  const timer = setTimeout(() => {
+    // 当抽屉关闭时重置表单
+    productStore.resetFormData();
+    // 去除预览禁用
+    drawer.value.disabled = false;
+    // 清除定时器
+    timer && clearTimeout(timer);
+  }, 100);
 };
 // #endregion
+
+// 设置行样式
+const getRowClassName = ({ row }: { row: { productStatus: number } }) => {
+  return row.productStatus ? 'disabled-row' : '';
+};
 </script>
 
 <style scoped lang="scss">
