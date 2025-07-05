33e8464e49864d8c4b3a1f6257850207bdf0793f
Cai <2945245791@qq.com>
2025-07-05 14:28:41 +0800

    refactor: 重构Card组件

--- src/components/Card/index.vue
+++ src/components/Card/index.vue
@@ -1,42 +1,49 @@
 <template>
-  <el-card
-    class="card-container"
-    body-class="card-body"
-    :shadow="props.shadow"
+  <component
+    :is="h(ElCard, { ...$attrs, ...props, ref: changeRef }, $slots)"
+    :style="{
+      border: props.border,
+      borderRadius: props.borderRadius,
+      backgroundColor: props.bgColor,
+    }"
     :body-style="{
       gap: `${props.gap}px`,
       flexDirection: props.flex,
-      padding: `${props.padding}px`,
-      backgroundColor: props.bgColor,
+      padding: props.padding,
     }"
-  >
-    <template #header v-if="$slots.header">
-      <slot name="header"></slot>
-    </template>
-    <slot></slot>
-    <template #footer v-if="$slots.footer">
-      <slot name="footer"></slot>
-    </template>
-  </el-card>
+    class="card-container"
+    body-class="card-body"
+  />
 </template>
 
 <script setup lang="ts">
-import { withDefaults } from 'vue';
+import { ElCard, type CardProps } from 'element-plus';
+import { h, getCurrentInstance, withDefaults } from 'vue';
 
-interface CardProps {
+interface MyCardProps extends Partial<CardProps> {
   shadow?: 'always' | 'hover' | 'never';
   bgColor?: string;
   flex?: 'row' | 'column';
   gap?: number;
-  padding?: number;
+  padding?: string;
+  border?: string;
+  borderRadius?: string;
 }
 
-const props = withDefaults(defineProps<CardProps>(), {
+// 获取当前组件实例，用于暴露实例方法
+const vm: any = getCurrentInstance();
+function changeRef(dialogInstance: any) {
+  // 将实例挂载到组件实例上，便于父组件调用
+  vm.exposeProxy = vm.exposed = dialogInstance || {};
+}
+
+const props = withDefaults(defineProps<MyCardProps>(), {
   shadow: 'never',
   bgColor: '#f5f7fa',
   flex: 'column',
   gap: 10,
-  padding: 15,
+  padding: '15px',
+  border: 'none',
 });
 </script>
 <script lang="ts">
@@ -47,11 +54,9 @@ export default {
 
 <style lang="scss">
 .card-container {
-  width: 100%;
-  border: none;
-
   .card-body {
     display: flex;
+    height: 100%;
   }
 }
 </style>
