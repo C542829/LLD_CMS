f3a6bfb553fab5ed666536857ddffa2c783b01f0
Cai <2945245791@qq.com>
2025-07-07 17:16:17 +0800

    feat: tab切换渲染组件，并记录当前tab激活项

--- src/components/ChildNav/index.vue
+++ src/components/ChildNav/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <component :is="h(ElTabs, { ...$attrs, ...props, ref: changeRef }, $slots)" type="border-card" class="demo-tabs">
-    <el-tab-pane v-for="item in props.navList" :key="item.label">
+  <component
+    :is="h(ElTabs, { ...$attrs, ...props, ref: changeRef }, $slots)"
+    v-model="activeName"
+    @tab-click="handleTabClick"
+    type="border-card"
+    class="demo-tabs"
+  >
+    <el-tab-pane v-for="item in props.navList" :key="item.label" :name="item.label" style="height: 100%">
       <template #label>
         <span class="custom-tabs-label">
           <el-icon v-if="item.icon">
@@ -9,14 +15,18 @@
           <span>{{ item.label }}</span>
         </span>
       </template>
-      <component :is="item.component" />
+
+      <!-- 仅当标签页激活时渲染组件 -->
+      <keep-alive>
+        <component :is="item.component" v-if="activeName === item.label" />
+      </keep-alive>
     </el-tab-pane>
   </component>
 </template>
 
 <script setup lang="ts">
 import { ElTabs, type TabsProps } from 'element-plus';
-import { h, getCurrentInstance, withDefaults } from 'vue';
+import { ref, onMounted, h, getCurrentInstance, withDefaults } from 'vue';
 
 interface NavItem {
   label: string;
@@ -29,6 +39,26 @@ interface MyTabsProps extends Partial<TabsProps> {
 }
 
 const props = withDefaults(defineProps<MyTabsProps>(), {});
+
+// 追踪当前激活的标签页
+const activeName = ref('');
+
+// 页面加载时，从 localStorage 恢复激活的 tab
+onMounted(() => {
+  const savedTab = localStorage.getItem('activeTab');
+  if (savedTab && props.navList.some((item) => item.label === savedTab)) {
+    activeName.value = savedTab;
+  } else {
+    // 默认激活第一个 tab
+    activeName.value = props.navList[0]?.label || '';
+  }
+});
+
+// tab 切换时保存到 localStorage
+const handleTabClick = (tab: any) => {
+  localStorage.setItem('activeTab', tab.props.name);
+  activeName.value = tab.props.name;
+};
 
 // // 获取当前组件实例，用于暴露对话框方法
 const vm: any = getCurrentInstance();
@@ -46,6 +76,7 @@ export default {
 <style scoped lang="scss">
 .demo-tabs {
   border: none;
+  height: 100%;
 
   > .el-tabs__content {
     padding: 32px;
