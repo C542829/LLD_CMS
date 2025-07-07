4a9d7e93165836e069b924fe27d24e7abd898a6d
Cai <2945245791@qq.com>
2025-07-07 17:14:18 +0800

    fix: 添加空数据提示

--- src/utils/feedback.ts
+++ src/utils/feedback.ts
@@ -12,6 +12,11 @@ interface ResponseType {
 }
 
 export const parseReqInform = (res: any) => {
+  if (!res) {
+    $Message.error('响应数据为空');
+    return false;
+  }
+
   try {
     if (res.code === ResponseCode.SUCCESS) {
       $Notification.success(res.data as string); // 显示成功消息
@@ -28,8 +33,13 @@ export const parseReqInform = (res: any) => {
 };
 
 export const parseReqList = (res: any, msg = '获取数据列表失败') => {
+  if (!res) {
+    $Message.error('响应数据为空');
+    return [];
+  }
+
   try {
-    if (res.code !== ResponseCode.SUCCESS) {
+    if (res?.code !== ResponseCode.SUCCESS) {
       $Notification.error(`${res.message}：${res.data}`);
     }
 
