9e4e426c9ffd69349840025d14bba909a7c8c719
Cai <2945245791@qq.com>
2025-07-14 16:01:24 +0800

    feat: 添加全局loading属性

--- src/store/modules/acl/setting.ts
+++ src/store/modules/acl/setting.ts
@@ -1,14 +1,18 @@
-// layout组件相关配置仓库
+// 设置配置仓库
 import { defineStore } from 'pinia';
 
 const useLayOutSettingStore = defineStore('SettingStore', {
   state: () => {
     return {
-      fold: false, // 用户控制菜单折叠还是收起控制
-      refresh: false, // 仓库这个属性用于控制刷新效果
+      fold: false, // 控制菜单折叠还是收起控制
+      refresh: false, // 控制刷新
+      loading: false, // 加载状态控制
+      loadingMsg: '加载中...', // 加载提示文字
     };
   },
   actions: {},
 });
 
 export default useLayOutSettingStore;
+
+export const useSettingStore = useLayOutSettingStore;
