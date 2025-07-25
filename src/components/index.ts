8c23c44f0d8fdb8cff87c8ff85811f2a387194b2
Cai <2945245791@qq.com>
2025-07-26 00:49:29 +0800

    refactor(components): 移除未使用的Category组件并清理注释格式
    清理全局组件注册文件，移除不再使用的Category组件
    统一注释格式，删除多余空格
    保持原有功能不变，仅进行代码整理

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
-//引入项目中全部的全局组件
+// 引入项目中全部的全局组件
 import SvgIcon from './SvgIcon/index.vue';
 import Pagination from './Pagination/index.vue';
-import Category from './Category/index.vue';
 import Dialog from './Dialog/index.vue';
 import Drawer from './Drawer/index.vue';
 import Table from './Table/index.vue';
@@ -15,14 +14,13 @@ import Message from './Message';
 import MessageBox from './MessageBox';
 import Notification from './Notification';
 
-//引入element-plus提供全部图标组件
+// 引入element-plus提供全部图标组件
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
-//全局对象
+// 全局对象
 const allGlobalComponent: any = {
   SvgIcon,
   Pagination,
-  Category,
   Dialog,
   Drawer,
   Table,
@@ -34,22 +32,22 @@ const allGlobalComponent: any = {
   DatePicker,
 };
 
-//对外暴露插件对象
+// 对外暴露插件对象
 export default {
-  //务必叫做install方法
+  // 务必叫做install方法
   install(app: any) {
-    //注册项目全部的全局组件
+    // 注册项目全部的全局组件
     Object.keys(allGlobalComponent).forEach((key) => {
-      //注册为全局组件
+      // 注册为全局组件
       app.component(key, allGlobalComponent[key]);
     });
 
-    //将element-plus提供图标注册为全局组件
+    // 将element-plus提供图标注册为全局组件
     for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
       app.component(key, component);
     }
 
-    //添加全局属性
+    // 添加全局属性
     app.provide('$Message', Message);
     app.provide('$MessageBox', MessageBox);
     app.provide('$Notification', Notification);
