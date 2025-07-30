4711994e1ae7afcafb98d9f98ea2ff53103563f9
Cai <2945245791@qq.com>
2025-07-30 16:56:33 +0800

    refactor(Dialog): 重构对话框组件使用动态组件和组合式API
    简化对话框组件实现，移除冗余代码，使用动态组件和组合式API重构
    通过h函数和$attrs/$slots实现更简洁的组件逻辑
    暴露组件实例方法便于父组件调用

--- src/components/Dialog/index.vue
+++ src/components/Dialog/index.vue
@@ -1,110 +1,26 @@
 <template>
-  <div class="custom-dialog">
-    <el-dialog
-      v-model="visible"
-      :title="props.title"
-      :width="props.width"
-      :top="props.top"
-      :modal="props.modal"
-      :append-to-body="props.appendToBody"
-      :lock-scroll="props.lockScroll"
-      :close-on-click-modal="props.closeOnClickModal"
-      :close-on-press-escape="props.closeOnPressEscape"
-      :show-close="props.showClose"
-      :before-close="handleClose"
-      :center="props.center"
-      :destroy-on-close="props.destroyOnClose"
-      :draggable="props.draggable"
-      :fullscreen="props.fullscreen"
-      :align-center="props.alignCenter"
-      :custom-class="props.customClass"
-    >
-      <slot></slot>
-      <template #footer v-if="$slots.footer">
-        <slot name="footer"></slot>
-      </template>
-    </el-dialog>
-  </div>
+  <component :is="h(ElDialog, { ...$attrs, ...props, ref: changeRef }, $slots)" />
 </template>
 
 <script lang="ts" setup>
-import { ref, watch, withDefaults } from 'vue';
+import { ElDialog, type DialogProps } from 'element-plus';
+import { h, getCurrentInstance, withDefaults } from 'vue';
 
-// 定义事件
-const $emit = defineEmits(['update:modelValue', 'open', 'opened', 'close', 'closed']);
+// 获取当前组件实例，用于暴露实例方法
+const vm: any = getCurrentInstance();
+function changeRef(instance: any) {
+  // 将实例挂载到组件实例上，便于父组件调用
+  vm.exposeProxy = vm.exposed = instance || {};
+}
 
-// 使用 withDefaults 添加默认值
-const props = withDefaults(
-  defineProps<{
-    modelValue: boolean;
-    title?: string;
-    width?: string | number;
-    top?: string;
-    modal?: boolean;
-    appendToBody?: boolean;
-    lockScroll?: boolean;
-    closeOnClickModal?: boolean;
-    closeOnPressEscape?: boolean;
-    showClose?: boolean;
-    center?: boolean;
-    destroyOnClose?: boolean;
-    draggable?: boolean;
-    fullscreen?: boolean;
-    alignCenter?: boolean;
-    customClass?: string;
-  }>(),
-  {
-    title: '',
-    width: '30%',
-    top: '15vh',
-    modal: true,
-    appendToBody: false,
-    lockScroll: true,
-    closeOnClickModal: true,
-    closeOnPressEscape: true,
-    showClose: true,
-    center: false,
-    destroyOnClose: false,
-    draggable: false,
-    fullscreen: false,
-    alignCenter: false,
-    customClass: '',
-  },
-);
+interface CustomProps extends Partial<DialogProps> {}
 
-// 内部可见性状态
-const visible = ref(props.modelValue);
-
-// 监听 modelValue 变化
-watch(
-  () => props.modelValue,
-  (newValue) => {
-    visible.value = newValue;
-  },
-);
-
-// 监听内部状态变化，通知父组件更新
-watch(
-  () => visible.value,
-  (newValue) => {
-    $emit('update:modelValue', newValue);
-    if (newValue) {
-      $emit('open');
-    } else {
-      $emit('close');
-    }
-  },
-);
-
-// 关闭前的回调
-const handleClose = (done: () => void) => {
-  $emit('close');
-  done();
+const props = defineProps<CustomProps>();
+</script>
+<script lang="ts">
+export default {
+  name: 'CustomDialog',
 };
 </script>
 
-<style scoped>
-.custom-dialog {
-  /* 自定义样式 */
-}
-</style>
+<style scoped></style>
