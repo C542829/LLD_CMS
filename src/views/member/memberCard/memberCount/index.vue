2547c1fcba5a54808a0c1295df1e40a727727111
Cai <2945245791@qq.com>
2025-08-03 01:27:13 +0800

    feat(会员统计): 新增会员统计模块及页面功能
    - 添加会员统计的Pinia store模块，包含搜索、分页和重置功能
    - 实现会员统计页面，包含搜索表单、数据表格和资产详情弹窗
    - 优化表格展示和分页逻辑，移除无用代码

--- src/views/member/memberCard/memberCount/index.vue
+++ src/views/member/memberCard/memberCount/index.vue
@@ -3,27 +3,16 @@
     <!-- 数据筛选 -->
     <Card class="operation-card">
       <div class="search-container">
-        <div class="search-item" v-if="false">
-          <label for="staffStatus" class="search-label">选择店铺：</label>
-          <el-select v-model="store.searchParams.storeId" id="staffStatus" style="width: 120px" placeholder="Select">
-            <el-option
-              v-for="item in [{ value: 1, label: '' }]"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            />
-          </el-select>
-        </div>
         <div class="search-item">
           <label>
             时间：
-            <DatePicker v-model="store.totalParams.date" style="width: 260px" />
+            <DatePicker v-model="store.search.date" style="width: 260px" />
           </label>
         </div>
         <div class="search-item">
-          <el-input v-model="store.totalParams.amount" style="max-width: 210px" placeholder="输入金额" clearable>
+          <el-input v-model="store.search.amount" style="width: 220px" placeholder="输入金额" clearable>
             <template #prepend>
-              <el-select v-model="store.totalParams.amountType" style="width: 130px">
+              <el-select v-model="store.search.amountType" style="width: 130px">
                 <el-option label="余额大于等于" :value="0" />
                 <el-option label="余额小于等于" :value="1" />
               </el-select>
@@ -34,97 +23,97 @@
           <el-button type="primary" @click="search">搜索</el-button>
         </div>
         <div class="search-item">
-          <el-button type="success" @click="">导出所有会员</el-button>
+          <el-button type="info" @click="store.resetSearch">重置</el-button>
+        </div>
+        <div class="search-item">
+          <el-button type="success" @click="exportAllMember">导出所有会员</el-button>
         </div>
       </div>
     </Card>
 
     <!-- 数据列表 -->
-    <Card padding="15px 15px 0 15px">
+    <Card padding="0">
       <PaginationTable
-        v-loading="store.isLoading"
-        :data="store.totalRecord.data"
-        :total="store.totalRecord.total"
-        v-model:currentPage="store.totalParams.currentPage"
-        v-model:pageSize="store.totalParams.pageSize"
+        v-loading="settingStore.loading && !dialog.visible"
+        :element-loading-text="settingStore.loadingMsg"
+        :data="store.tableData.list"
+        :total="store.tableData.total"
+        v-model:currentPage="store.search.pageNum"
+        v-model:pageSize="store.search.pageSize"
         @size-change="handleSizeChange"
         @pagination-current-change="handleCurrentChange"
       >
-        <el-table-column prop="infoName" label="姓名" min-width="80" />
-        <el-table-column prop="infoGender" label="性别" width="60" :formatter="sexMap" />
-        <el-table-column prop="infoCardNumber" label="卡号" min-width="80" />
-        <el-table-column prop="infoPhoneNumber" label="手机号" width="120" />
-        <el-table-column prop="infoLastConsumptionTime" label="末次消费" width="170" />
-        <el-table-column prop="infoLastRechargeTime" label="末次充值" width="170" />
-        <el-table-column prop="remark" label="备注" min-width="50" />
+        <el-table-column prop="name" label="姓名" min-width="60" />
+        <el-table-column prop="gender" label="性别" min-width="30" :formatter="sexMap" />
+        <el-table-column prop="cardNumber" label="卡号" min-width="80" />
+        <el-table-column prop="phoneNumber" label="手机号" min-width="80" />
+        <el-table-column prop="balance" label="店内总余额" min-width="80" :formatter="amountFormatter" />
+        <el-table-column prop="lastConsumptionTime" label="末次消费日期" min-width="80" />
+        <el-table-column prop="lastRechargeTime" label="末次充值日期" min-width="80" />
         <el-table-column label="操作" min-width="80">
-          <template #default="scope">
-            <el-button link type="primary" @click="showDialog(scope.row)">资产详情</el-button>
+          <template #default="{ row }">
+            <el-button link type="primary" @click="showDialog(row)">资产详情</el-button>
           </template>
         </el-table-column>
       </PaginationTable>
     </Card>
   </div>
 
-  <el-dialog v-model="dialog.visible" :title="dialog.title" width="800px">
-    <PropertyDetail @close-dialog="dialog.visible = false"></PropertyDetail>
+  <el-dialog v-model="dialog.visible" :title="dialog.title" center width="800px">
+    <PropertyDetail :memberId="dialog.memberId" @close-dialog="dialog.visible = false" />
   </el-dialog>
 </template>
 
 <script setup lang="ts">
-import PropertyDetail from '../PropertyDetail.vue';
-import { ref, reactive, onMounted, onUnmounted } from 'vue';
-import { sexMap } from '@/enums/mapFormatter';
+import { reactive, onMounted } from 'vue';
+import { sexMap, amountFormatter } from '@/utils/formatter';
+
+import PropertyDetail from '../components/PropertyDetail.vue';
 
 // 引入数据仓库
-import { useMemberStore } from '@/store/modules/member/member';
-const store = useMemberStore();
+import { useSettingStore } from '@/store/modules/acl/setting';
+import { useMemberCountStore } from '@/store/modules/member/memberCount';
+const settingStore = useSettingStore();
+const store = useMemberCountStore();
 
 // 初始化
 onMounted(() => {});
 
-onUnmounted(() => {
-  store.isLoading = false;
-});
-
 // 搜索
 const search = () => {
-  store.setTotalRecord();
+  store.setTableData();
 };
 
 // 处理分页变化
 const handleSizeChange = (val: number) => {
-  store.activeParams.pageSize = val;
-  store.setTotalRecord();
+  store.search.pageSize = val;
+  store.setTableData();
 };
 
 const handleCurrentChange = (val: number) => {
-  store.activeParams.currentPage = val;
-  store.setTotalRecord();
+  store.search.pageNum = val;
+  store.setTableData();
 };
 
 // 模态框
-const dialog: any = reactive({
+const dialog = reactive({
   title: '优惠券列表',
   visible: false,
+  memberId: 0,
 });
 
 // 打开模态框
 const showDialog = (row: any) => {
   // 显示模态框
-  dialog.title = `会员${row.infoName}在本店的资产详情`;
+  dialog.title = `会员${row.name}在本店的资产详情`;
   dialog.visible = true;
-
-  // 表单数据回显
-  store.formData = row;
+  dialog.memberId = row.id;
 };
+
+const exportAllMember = () => {};
 </script>
 
 <style scoped lang="scss">
-.table-main {
-  height: 100%;
-}
-
 :deep(.el-input-group__append .el-button--primary) {
   @include primary-button;
 }
