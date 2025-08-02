94ef54e1e595951fce80fcb802e1681ac5c176e5
Cai <2945245791@qq.com>
2025-08-03 01:41:01 +0800

    refactor(store): 更新会员列表API导入路径
    将会员列表API的导入路径从member/memberList更新为member/member，保持与后端API路径一致

--- src/store/modules/order/index.ts
+++ src/store/modules/order/index.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia';
 import { reactive, ref } from 'vue';
-import { reqVipList } from '@/api/member/memberList';
+import { reqVipList } from '@/api/member/member';
 import { parseResList, parseResObj, parseResMsg } from '@/utils/parseResponse';
 
 export const useOrderStore = defineStore('Order', () => {
