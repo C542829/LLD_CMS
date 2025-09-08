59efae0362fd73931b1600f65ed11aeb1fe9d9ec
Cai <2945245791@qq.com>
2025-09-08 08:45:55 +0800

    refactor(BarChart): 移除未使用的vue导入以优化代码
    清理了未使用的defineProps和defineEmits导入，保持代码简洁

--- src/views/home/components/BarChart.vue
+++ src/views/home/components/BarChart.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted, watch, defineProps, defineEmits } from 'vue';
+import { ref, onMounted, onUnmounted, watch } from 'vue';
 import * as echarts from 'echarts';
 import { ElLoading } from 'element-plus';
 import 'element-plus/es/components/loading/style/css';
