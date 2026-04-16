ee93db53d13f3c28ab00cc3a23d3f2dbca479297
Cai <2945245791@qq.com>
2026-04-16 20:11:58 +0800

    fix(会员列表): 调整门店选择器宽度并添加最大折叠标签数限制
    将门店选择器的宽度从120调整为100，并添加maxCollapseTags属性限制折叠标签数量为0，以优化界面显示效果

--- src/views/member/memberCard/memberList/index.vue
+++ src/views/member/memberCard/memberList/index.vue
@@ -13,8 +13,9 @@
               <OrgSelect
                 v-model="store.search.orgIds"
                 placeholder="门店"
-                class="w-120"
+                class="w-100"
                 :multiple="true"
+                :maxCollapseTags="0"
                 @change="search"
                 @clear="search"
               />
