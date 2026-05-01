a9ba170dd43a61404496916b65687dddc55c2787
Cai <2945245791@qq.com>
2026-05-01 22:45:01 +0800

    feat: 注释掉登出后的路由跳转

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -70,7 +70,7 @@ const fullScreen = () => {
 const logout = async () => {
   await userStore.userLogout();
   //跳转到登录页面
-  $router.push({ path: '/login', query: { redirect: $route.path } });
+  // $router.push({ path: '/login', query: { redirect: $route.path } });
 };
 
 const getOrgName = computed(() => {
