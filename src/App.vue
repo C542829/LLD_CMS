9a3dff29ed809add39b97f1ae7c60a1722ec0d80
Cai <2945245791@qq.com>
2026-05-13 22:11:30 +0800

    style: 格式化 el-dialog 属性换行

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
     <router-view></router-view>
 
     <!-- 版本更新提示 -->
-    <el-dialog v-model="versionCheck.hasUpdate.value" title="系统更新" width="400px" :close-on-click-modal="false" :close-on-press-escape="false" :show-close="false">
+    <el-dialog
+      v-model="versionCheck.hasUpdate.value"
+      title="系统更新"
+      width="400px"
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+      :show-close="false"
+    >
       <span>系统已发布新版本，请刷新页面以获取最新内容。</span>
       <template #footer>
         <el-button type="primary" @click="versionCheck.refresh()">立即刷新</el-button>
