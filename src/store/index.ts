7afe03af0284b2628cfd5e8bcf5fa7d2ae572d77
Cai <2945245791@qq.com>
2026-06-20 00:29:00 +0800

    refactor(store): 移除未使用的响应式状态

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,4 @@
-import { reactive, readonly, ref } from 'vue';
 import { createPinia, type Pinia } from 'pinia';
 const pinia = createPinia();
 
 export default pinia;
-
-// 用户信息
-let userInfo_ = {} as UserInfo;
-const storeUserInfo_ = reactive(userInfo_);
-/** 用户信息 */
-export const storeUserInfo = readonly(storeUserInfo_);
-export function setStoreUserInfo(val: Partial<UserInfo>) {
-  Object.assign(storeUserInfo_, val);
-}
-
-// 门店信息
-let orgInfo_ = {} as OrgInfo;
-const storeOrgInfo_ = reactive(orgInfo_);
-/** 门店信息 */
-export const storeOrgInfo = readonly(storeOrgInfo_);
-export function setStoreOrgInfo(val: Partial<OrgInfo>) {
-  Object.assign(storeOrgInfo_, val);
-}
